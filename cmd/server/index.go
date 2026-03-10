a0721322952378b961ec051203b9d9142c29c22a
masar3141 <masar314@proton.me>
2026-03-10 10:24:59 +0100

    display path

--- cmd/server/index.go
+++ cmd/server/index.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"path/filepath"
 )
 
 func (a *application) handleGetIndex(w http.ResponseWriter, r *http.Request) {
-	tpl, err := template.ParseFiles(filepath.Join(a.cfg.tplDir, "index.html"))
+	path := filepath.Join(a.cfg.tplDir, "index.html")
+	tpl, err := template.ParseFiles(path)
+	fmt.Println(path)
 	if err != nil {
 		a.logger.Error("Can't create template hello", "error", err.Error())
 	}
